0504642483551af050085c7de67da4a2d29ddecd
elf <filonenko0406@gmail.com>
2019-11-26 21:54:55 +0800

    fix

--- parts/emails/service.php
+++ parts/emails/service.php
@@ -1,10 +1,4 @@
 <?php
-/*	use PHPMailer\PHPMailer\PHPMailer;
-	use PHPMailer\PHPMailer\Exception;
-
-	require '../libs/PHPMailer/src/Exception.php';
-	require '../libs/PHPMailer/src/PHPMailer.php';
-
 	function getServiceForm(){
 		parse_str($_POST['data'], $data);
 
@@ -18,6 +12,8 @@
 		$date = $data['date'];
 		$time = $data['time'];
 
+		$to      = 'filonenko0406@gmail.com';
+		$subject = 'Новая запись с сайта';
 		$message = '';
 		$message .= 'Пользователь по имени '.$name.$newLine;
 		$message .= 'Телефон: ' . $phone . $newLine;
@@ -27,25 +23,9 @@
 		$message .= 'Дата: ' . $date . $newLine;
 		$message .= 'Время:' . $time . $newLine;
 
-		$mail = new PHPMailer(true);
-
-		$mail->setFrom('filonenko0406@gmail.com', 'Your Name');
-		$mail->addAddress('filonenko0406@gmail.com', 'My Friend');
-		$mail->Subject  = 'Новая запись с сайта Лечись';
-		$mail->Body     = $message;
-		$mail->addAttachment($_FILES['photo_1']['tmp_name'], $_FILES['photo_1']['name']);
-		$mail->addAttachment($_FILES['photo_2']['tmp_name'], $_FILES['photo_1']['name']);
-		$mail->addAttachment($_FILES['photo_3']['tmp_name'], $_FILES['photo_1']['name']);
-		$mail->addAttachment($_FILES['photo_4']['tmp_name'], $_FILES['photo_1']['name']);
-
-		if(!$mail->send()) {
-			echo 'Message was not sent.';
-			echo 'Mailer error: ' . $mail->ErrorInfo;
-		} else {
-			echo 'Message has been sent.';
-		}
+		mail($to, $subject, $message);
 	}
 
 	add_action('wp_ajax_nopriv_service', 'getServiceForm' );
 	add_action('wp_ajax_service', 'getServiceForm' );
-*/?>
+?>
